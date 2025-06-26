31a8bfec6c6e2ebed00d88689ef1962d681181a6
pankaj_anand <pankajanand150899@gmail.com>
2025-06-26 09:47:07 +0530

    project added to git repo

---
+++ jsonhandler/src/test/java/com/anand/jsonhandler/JsonhandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anand.jsonhandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonhandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
